86d4e4f2701462820883954148fc13b37c4b6ad6
Diego Pires <diego.didigos@gmail.com>
2025-09-25 10:44:54 -0300

    revertido alterações no cors e ajustada porta para variável de ambiente

--- services/index.js
+++ services/index.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose'
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 const app = express()
-const port = process.env.PORT || 3000;
-
-//middleware
+const port = 3000
 app.use(express.json());
 app.use(cors());
 
