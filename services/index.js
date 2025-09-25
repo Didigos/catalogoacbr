aef471fdffaf4b73317583b02e5f44a75cce297c
Diego Pires <diego.didigos@gmail.com>
2025-09-25 10:22:18 -0300

    feat: ajusta configuração da porta para usar variável de ambiente

--- services/index.js
+++ services/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
